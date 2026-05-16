cbeca0a425fcc59dd4f5fa3bf013a2458461993d
Nän Ekman <nen_92@hotmail.com>
2026-05-16 23:33:28 +0200

    Add Google Auth support for web and Android

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -16,7 +16,20 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowExpoWeb", policy =>
     {
         policy
-            .WithOrigins("http://localhost:8081")
+            .WithOrigins(
+                "http://localhost:8081",
+                "http://127.0.0.1:8081",
+
+                // Näns Expo
+                // "http://192.168.68.104:8081"
+                "http://192.168.8.6:8081"
+
+                // Alexanders Expo
+                // "http://
+
+                // Farishtas Expo
+                // "http://
+        )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
